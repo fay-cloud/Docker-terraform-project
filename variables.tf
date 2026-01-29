58eaa70bf331bb844304520ad6d846b04897461a
Fayomi Kehinde <fayomik02@gmail.com>
2026-01-29 13:58:58 +0100

    Clean .gitignore and remove sensitive files from tracking

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
